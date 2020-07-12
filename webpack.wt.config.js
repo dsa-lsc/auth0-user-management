23d7c84ef1f318ddcf8eab6f5f0df1107f526bcc
Simon <contact@vansintjan.net>
2020-07-12 15:33:22 -0700

    Tries new things with webpack.wt.config.js

--- webpack.wt.config.js
+++ webpack.wt.config.js
@@ -5,9 +5,9 @@ var _       = require('lodash');
 
 var LIST_MODULES_URL = 'https://webtask.it.auth0.com/api/run/wt-tehsis-gmail_com-1?key=eyJhbGciOiJIUzI1NiIsImtpZCI6IjIifQ.eyJqdGkiOiJmZGZiOWU2MjQ0YjQ0YWYyYjc2YzAwNGU1NjgwOGIxNCIsImlhdCI6MTQzMDMyNjc4MiwiY2EiOlsiZDQ3ZDNiMzRkMmI3NGEwZDljYzgwOTg3OGQ3MWQ4Y2QiXSwiZGQiOjAsInVybCI6Imh0dHA6Ly90ZWhzaXMuZ2l0aHViLmlvL3dlYnRhc2tpby1jYW5pcmVxdWlyZS90YXNrcy9saXN0X21vZHVsZXMuanMiLCJ0ZW4iOiIvXnd0LXRlaHNpcy1nbWFpbF9jb20tWzAtMV0kLyJ9.MJqAB9mgs57tQTWtRuZRj6NCbzXxZcXCASYGISk3Q6c';
 
-module.exports = Request.get(LIST_MODULES_URL, { json: true }).then(function (data) {
+module.exports = Request(LIST_MODULES_URL, { json: true }).then(function (data) {
   var modules = data.modules;
-
+  console.log('modules', modules)
   return {
     entry: './webtask',
     output: {
